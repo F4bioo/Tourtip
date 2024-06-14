60217c476c8e5e382085428763d80654efb4943c
GitHub Action <action@github.com>
2024-06-14 19:21:57 +0000

    Changed: Bump app version v1.06 build 11

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -5,8 +5,8 @@ object Config {
     const val applicationId: String = packageName
     const val minSdk: Int = 21
     const val targetSdk: Int = 34
-    const val versionCode: Int = 10
-    const val versionName: String = "1.2024.06.14"
+    const val versionCode: Int = 11
+    const val versionName: String = "1.06"
 
     val artifactId: String
         get() = packageName.substringAfterLast(delimiter = ".")
