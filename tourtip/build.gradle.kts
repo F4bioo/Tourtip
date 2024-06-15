e05de612574a74b6c1e9782236cf30fb3a7610f1
F4bioo <costa.fbo@gmail.com>
2024-06-15 01:30:44 -0300

    Added: Publish Tourtip library to Maven Central

--- tourtip/build.gradle.kts
+++ tourtip/build.gradle.kts
@@ -2,7 +2,9 @@ plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsKotlinAndroid)
     id("maven-publish")
+    id("signing")
 }
+
 apply(from = "$rootDir/plugins/android-build.gradle")
 
 android {
@@ -22,14 +24,57 @@ publishing {
             artifactId = Config.artifactId
             groupId = Config.groupId
             version = Config.version
+
+            pom {
+                name.set("Tourtip")
+                description.set("A guided tour library for Android applications, built using Jetpack Compose.")
+                url.set("https://github.com/F4bioo/Tourtip")
+
+                licenses {
+                    license {
+                        name.set("The MIT License")
+                        url.set("https://mit-license.org/")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("F4bioo")
+                        name.set("Fabio Marinho")
+                        email.set("costa.fbo@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:github.com/F4bioo/Tourtip.git")
+                    developerConnection.set("scm:git:ssh://github.com/F4bioo/Tourtip.git")
+                    url.set("https://github.com/F4bioo/Tourtip")
+                }
+            }
         }
     }
     repositories {
+        maven {
+            name = "OSSRH"
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = project.findProperty("ossrhUsername") as String?
+                    ?: System.getenv("OSSRH_USERNAME")
+                password = project.findProperty("ossrhPassword") as String?
+                    ?: System.getenv("OSSRH_PASSWORD")
+            }
+        }
         maven {
             name = "local"
             url = uri(layout.buildDirectory.dir("repo"))
         }
     }
+}
+
+signing {
+    useInMemoryPgpKeys(
+        System.getenv("PGP_PRIVATE_KEY"),
+        System.getenv("PGP_PASSPHRASE")
+    )
+    sign(publishing.publications["release"])
 }
 
 val resolvedImplementation: Configuration by configurations.creating {
